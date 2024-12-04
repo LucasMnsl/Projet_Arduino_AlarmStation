f5554c3e8515039b8cbfb19f0ad1eba63c84cdd6
LucasMnsl <lucas.mansilha41@gmail.com>
2024-12-04 09:01:25 +0100

    .ino ok

---
+++ AlarmStation/AlarmStation.ino
@@ -0,0 +1,15 @@
+#include "Application.h"
+
+Application myApplication(13);
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  myApplication.init();
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  myApplication.run();
+}
