1255cf3843749c292261cb90a8301b1f3d667220
LucasMnsl <lucas.mansilha41@gmail.com>
2024-12-04 11:08:43 +0100

    commentaires ok pour tout le code

--- AlarmeStation/Application.h
+++ AlarmeStation/Application.h
@@ -1,6 +1,8 @@
 /*********************************************************************
  * @file  Apllication.cpp
- * @author <MANSILHA Lucas mansilha@insa-toulouse.fr, LIN Hoakai halin@insa-toulouse.fr>
+ * @author 
+ *        MANSILHA Lucas <mansilha@insa-toulouse.fr> 
+ *        LIN Hoakai <halin@insa-toulouse.fr>
  * @brief Fichier source de l'application
  *********************************************************************/
 #ifndef APPLICATION_H_
@@ -11,49 +13,62 @@
 #include "MENU.h"
 
 /**
-  * @class Application
-  * @brief Classe qui gère l'ensemble des composants de l'alarme 
-*/    
+ * @class Application
+ * @brief Classe principale qui orchestre les interactions entre les différents 
+ *        composants d'une alarme : LED, buzzer et menu utilisateur.
+ */   
 class Application
 {
   private :
-    int maPinBUTTON1;
-    int maPinBUTTON2;
-    MENU *monMENU;
-    int maPinLED;
-    LED *maLED;         //pointeur sur un objet LED permet a ce que toute la classe puisse accéder à l'objet mais sans le créer maintenant
-    int maPinBUZZER;
-    BUZZER *monBUZZER;
+  
+    int maPinBUTTON1;       /**< Numéro de la broche associée au bouton 1. */
+    int maPinBUTTON2;       /**< Numéro de la broche associée au bouton 2. */
+    MENU *monMENU;          /**< Pointeur vers un objet MENU pour gérer l'interface utilisateur. */
+
+    int maPinLED;           /**< Numéro de la broche associée à la LED. */
+    LED *maLED;             /**< Pointeur vers un objet LED, pour permettre un accès global à l'objet sans le créer immédiatement. */
+
+    int maPinBUZZER;        /**< Numéro de la broche associée au buzzer. */
+    BUZZER *monBUZZER;      /**< Pointeur vers un objet BUZZER, pour permettre un accès global à l'objet sans le créer immédiatement. */
+    
+
+    //pointeur sur un objet permet a ce que toute la classe puisse accéder à l'objet mais sans le créer maintenant car le constructeur sera appelé en dehors du setup
+    
   public :
   
     /**
-     * @fn Application();
-     * @brief Constructeur par defaut
-    */  
+     * @brief Constructeur par défaut de la classe Application.
+     * Ne fait rien.
+     */
     Application();
 
     /**
-      * @fn Application(int pinPourLED, int pinPourBUZZER);
-      * @brief Constructeur principal qui affecte les variables dédiée au pins et initialise les pointeurs d'objets
-    */  
+     * @brief Constructeur principal qui initialise les broches et les pointeurs pour les composants.
+     * @param pinPourLED Numéro de la broche pour la LED.
+     * @param pinPourBUZZER Numéro de la broche pour le buzzer.
+     * @param pinPourBUTTON1 Numéro de la broche pour le bouton 1.
+     * @param pinPourBUTTON2 Numéro de la broche pour le bouton 2.
+     * Les pointeurs sont initialisé à NULL pour éviter de créer des objets en dehors du setup du fichier .ino
+     */
     Application(int pinPourLED, int pinPourBUZZER, int pinPourBUTTON1, int pinPourBUTTON2);
     
     /**
-     * @fn ~Application();
-     * @brief Destructeur, il détruit les pointeurs utilisés précédememnt
-    */    
+     * @brief Destructeur de la classe Application.
+     * Libère les ressources allouées dynamiquement, notamment les pointeurs des objets LED, BUZZER et MENU.
+     */   
     ~Application();
         
     /**
-     * @fn void init(void)
-     * @brief Fonction d'initialisation de l'application
-    */
+     * @brief Initialise l'application.
+     * Configure les composants nécessaires pour leur utilisation notamment 
+     *      la création des objets et initialisation de la liaison série.
+     */
     void init(void);
     
     /**
-     * @fn void run(void)
-     * @brief Fonction de lancement de l'application
-    */
+     * @brief Démarre l'application.
+     * Gère la boucle principale de l'application et coordonne les interactions entre les différents composants.
+     */
     void run(void);
 };
 #endif
--- AlarmeStation/BUZZER.h
+++ AlarmeStation/BUZZER.h
@@ -6,28 +6,94 @@
 #ifndef BUZZER_H_
 #define BUZZER_H_
 
+/**
+ * @class BUZZER
+ * @brief Classe pour gérer un buzzer, permettant de configurer et de
+ *        les fréquences, les délais et l'état du buzzer.
+ */
 class BUZZER {
   private :
-    bool etat;
-    int pinBUZZER;
-    int freq1;
-    int freq2;
-    int delai;
-    bool isFreq1;
-    unsigned long lastMillis=0;
-    unsigned long currentMillis=0;
+
+    bool etat;                        /**< État actuel du buzzer (true = actif, false = inactif). */
+    int pinBUZZER;                    /**< Numéro de la broche à laquelle le buzzer est connecté. */
+    int freq1;                        /**< Première fréquence pour le buzzer. */
+    int freq2;                        /**< Deuxième fréquence pour le buzzer. */
+    int delai;                        /**< Délai (en millisecondes) pour alterner entre les fréquences. */
+    bool isFreq1;                     /**< Indique si la fréquence actuelle est freq1 (true) ou freq2 (false). */
+    unsigned long lastMillis = 0;     /**< Dernière valeur enregistrée de millis() pour le timing. */
+    unsigned long currentMillis = 0;  /**< Valeur actuelle de millis() utilisée pour le timing. */
+    
   public :
+
+     /**
+     * @brief Constructeur par défaut de la classe BUZZER.
+     * Suppose que le buzzer est connecté à la broche 14.
+     * Affecte des valeurs par défaut à frequence 1 : 800Hz, frequence 2 : 1200Hz, delai : 300ms, isFreq1 : False
+     */
     BUZZER();
+
+     /**
+     * @brief Constructeur avec initialisation de la broche du buzzer.
+     * @param pin Numéro de la broche à laquelle le buzzer est connecté.
+     * Affecte des valeurs par défaut à frequence 1 : 800Hz, frequence 2 : 1200Hz, delai : 300ms, isFreq1 : False
+     */
     BUZZER(int pin);
+
+     /**
+     * @brief Destructeur de la classe BUZZER.
+     */
     ~BUZZER();
+
+     /**
+     * @brief Permet de récupérer l'état actuel du buzzer.
+     * @return true si le buzzer est actif, false s'il est inactif.
+     */
     bool getEtat();
+
+     /**
+     * @brief Permet de récupérer la première fréquence configurée pour le buzzer.
+     * @return La première fréquence (en Hertz).
+     */
     int getFreq1();
+
+     /**
+     * @brief Permet de récupérer la deuxième fréquence configurée pour le buzzer.
+     * @return La deuxième fréquence (en Hertz).
+     */
     int getFreq2();
+
+     /**
+     * @brief Permet de récupérer le delai configuré pour le buzzer.
+     * @return le delai (en millisecondes).
+     */
     int getDelai();
+
+     /**
+     * @brief Modifie la première fréquence du buzzer.
+     * @param f La première fréquence (en Hertz).
+     */
     void setFreq1(int f);
+
+     /**
+     * @brief Modifie la deuxième fréquence du buzzer.
+     * @param f La deuxième fréquence (en Hertz).
+     */
     void setFreq2(int f);
+
+     /**
+     * @brief Modifie le delai du buzzer.
+     * @param t le delai (en milliseconde).
+     */
     void setDelai(int t);
+
+     /**
+     * @brief Active le buzzer.
+     */
     void turnOn();
+
+     /**
+     * @brief Désactive le buzzer.
+     */
     void turnOff();
 };
 
