11900d22061cdf0b58102b1a85354bc2402f1be6
Haokai-LIN <halin@insa-toulouse.fr>
2024-12-08 10:29:33 +0100

    Add files via upload

---
+++ AlarmeStation/Capteur.cpp
@@ -0,0 +1,13 @@
+#include "Capteur.h"
+
+int Capteur::nombre_capteur = 0;
+
+int Capteur::lire_nombre_capteur(){
+  return nombre_capteur;
+}
+
+int Capteur::lire_etat(){
+  return etat;
+}
+
+
---
+++ AlarmeStation/CapteurRanger.cpp
@@ -0,0 +1,35 @@
+#include "Ultrasonic.h"
+#include "Capteur.h"
+#include "CapteurRanger.h"
+
+CapteurRanger::CapteurRanger(int l,int p){
+  limit = l;
+  if ((p == 2) or (p == 12) or (p == 14) or (p == 15)){
+    port = p;
+  }
+  else{
+    Serial.print("port incorret");
+  }
+  nombre_capteur++;
+}
+
+
+long CapteurRanger::lire_distance(){
+  return distance;
+}
+
+void CapteurRanger::check_etat(Ultrasonic *U){
+  distance = U->MeasureInCentimeters(); 
+  if (distance <= limit){
+    etat = 1;
+  }
+  else{
+    etat = 0;
+  }
+}
+
+int CapteurRanger::lire_port(){
+  return port;
+}
+
+
